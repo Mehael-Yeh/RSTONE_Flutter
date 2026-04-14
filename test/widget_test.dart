e0a36d66c2bbd5b6a3dde26ad61637abbc881663
Mehael-Yeh <minghaoye@foxmail.com>
2026-04-14 14:46:54 +0800

    Fix frontmatter tags parsing for markdown delimiters

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -39,5 +39,23 @@ tags:
       expect(item.folder, '产品配方');
       expect(item.getTableFields(), {'名称': 'RS1001-银'});
     });
+
+    test('does not parse frontmatter delimiter as tag', () {
+      const content = '''
+---
+tags:
+  - 水性
+  - PU
+  - 底漆
+  - 耐汽油
+  - 中漆
+---
+[[RD1160-黑]]
+''';
+
+      final item = ProductItem.fromMdContent('assets/产品列表/RD1160.md', content);
+
+      expect(item.tags, ['水性', 'PU', '底漆', '耐汽油', '中漆']);
+    });
   });
 }
